a1a8b0761e309d6d0ab154c9e701242c2fa25289
Anna Gareeva <gara@omegafuture.ru>
2022-04-01 18:09:59 +0300

    - first commit

---
+++ src/main/java/Praktikun.java
@@ -0,0 +1,16 @@
+public class Praktikun {
+
+
+        public static void main(String[] args) {
+            String name = "Тест тестович";
+            Account account = new Account(name);
+            account.checkNameToEmboss();
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+        }
+
+
+}
